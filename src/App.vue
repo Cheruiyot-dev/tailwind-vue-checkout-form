c2f1587dda3c1b9234260571e57a13e71eb67553
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-08-15 16:25:41 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CheckOutPage from './pages/CheckOutPage.vue';
+
+
+</script>
+
+<template>
+  <div   class="bg-gray-200">
+   <CheckOutPage/>
+  </div>
+  
+
+  
+</template>
+
+
---
+++ src/pages/CheckOutPage.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div>
+        <h3 class="text-red-500">Checkout page</h3>
+    </div>
+
+</template>
+
+<script >
+
+export default {
+    name: "CheckOutPage",
+    data() {
+        return {}
+
+    },
+    
+    methods: {}
+}
+
+</script>
